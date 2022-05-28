4854624355412e3b188ae46bfbae5a82ecb7385b
Amon Brollo <amonbrollo@gmail.com>
2022-05-28 19:28:27 -0300

    😤 Added an event listener for the button but I keep getting a 'button is not defined' error

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,26 @@
 // get input from user
+const input = document.querySelector('input');
+button.addEventListener('click', () => {
+    const userInput = input.value;
+    
+    // call the API
+    const url = `https://botw-compendium.herokuapp.com/api/v2/entry/${userInput}`;
 
-// call the API
+    async function getThing(url, doThis) {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            doThis(data);
+        }
+    }
 
-// find the information within the API
+    const thing = getThing(url, doStuff);
 
-// get the result back to the user: item's name, 
-// description, image and locations.
+    listJson.jsonContent = thing;
+
+    // find the information within the API
+
+    // get the result back to the user: item's name, 
+    // description, image and locations.
+    const thingInfo = document.createElement('span');
+});
