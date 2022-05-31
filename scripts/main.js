cf5295be0b3df0601737a11aa1eeaddb8cac18f9
Amon Brollo <amonbrollo@gmail.com>
2022-05-31 10:13:04 -0300

    😁 Made a test paragraph to see if my button would work by making a couple a changes. Success! Also commented out the test functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,37 @@
 // get input from user
-const input = document.querySelector('input');
-button.addEventListener('click', () => {
-    const userInput = input.value;
+const element = document.getElementById("submit-button");
+
+// Button test ↓
+// element.addEventListener("click", buttonTest);
+
+// function buttonTest() {
+//     document.getElementById("test").innerHTML = "This button works!😅";
+// };
+
+const userInput = element.addEventListener("click", getApi);
+
+// const input = document.querySelector('input');
+// button.addEventListener('click', () => {
+//     const userInput = input.value;
     
-    // call the API
-    const url = `https://botw-compendium.herokuapp.com/api/v2/entry/${userInput}`;
+//     // call the API
+//     const url = `https://botw-compendium.herokuapp.com/api/v2/entry/${userInput}`;
 
-    async function getThing(url, doThis) {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            doThis(data);
-        }
-    }
+//     async function getThing(url, doThis) {
+//         const response = await fetch(url);
+//         if (response.ok) {
+//             const data = await response.json();
+//             doThis(data);
+//         }
+//     }
 
-    const thing = getThing(url, doStuff);
+//     const thing = getThing(url, doStuff);
 
-    listJson.jsonContent = thing;
+//     listJson.jsonContent = thing;
 
     // find the information within the API
 
     // get the result back to the user: item's name, 
     // description, image and locations.
     const thingInfo = document.createElement('span');
-});
+// });
