6ee0f9531ec550f07f2ffb009fb01dac37934a6f
Amon <77292585+AmonBrollo@users.noreply.github.com>
2022-06-01 12:50:18 -0300

    🤯 With Schenatto's help, passed a function into another function and added code in the doStuff function to output the result in the page

--- scripts/main.js
+++ scripts/main.js
@@ -4,27 +4,24 @@ const element = document.getElementById("submit-button");
 function handleSubmitClick() {
     const inputElement = document.getElementById("userInput");
     const inputValue = inputElement.value
-    // console.log("O valor do input é: "+inputValue)
+
+    // call the API
+    const url = "https://botw-compendium.herokuapp.com/api/v2/entry/";
+    async function getThing() {
+        // find the information within the API
+        const response = await fetch(url+inputValue);
+        if (response.ok) {
+            const result = await response.json();
+            doStuff(result);
+        };
+    };
+    getThing()
 }
 element.addEventListener("click", handleSubmitClick)
 
-// call the API
-const url = "https://botw-compendium.herokuapp.com/api/v2/entry/";
-let results = null;
-const userInput = input.value;
-    async function getThing(url, doStuff) {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            doStuff(data);
-        };
-};
-
-    // find the information within the API
-
 // get the result back to the user: item's name, description, image and locations.
-
-function doStuff(data) {
-    results = data;
-    console.log("first: ", results);
+function doStuff(result) {
+    console.log(result);
+    const imageElement = document.getElementById("image")
+    imageElement.setAttribute("src",result.data.image)
 }
