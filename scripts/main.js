f8d5d852eba849781eefb0366cc37fb193b5a37c
Amon Brollo <amonbrollo@gmail.com>
2022-05-31 22:02:19 -0300

    🧐 Added handleSubmitClick function to get user's input

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,26 @@
 // get input from user
 const element = document.getElementById("submit-button");
 
-// Button test ↓
-// element.addEventListener("click", buttonTest);
+function handleSubmitClick() {
+    const inputElement = document.getElementById("userInput");
+    const inputValue = inputElement.value
+    // console.log("O valor do input é: "+inputValue)
+}
+element.addEventListener("click", handleSubmitClick)
 
-// function buttonTest() {
-//     document.getElementById("test").innerHTML = "This button works!😅";
-// };
-
-const userInput = element.addEventListener("click", getApi);
-
-// const input = document.querySelector('input');
-// button.addEventListener('click', () => {
+// call the API
+// const url = "https://botw-compendium.herokuapp.com/api/v2/entry/"
+// function getApi (url) {
 //     const userInput = input.value;
-    
-//     // call the API
-//     const url = `https://botw-compendium.herokuapp.com/api/v2/entry/${userInput}`;
-
 //     async function getThing(url, doThis) {
 //         const response = await fetch(url);
 //         if (response.ok) {
 //             const data = await response.json();
 //             doThis(data);
-//         }
-//     }
-
-//     const thing = getThing(url, doStuff);
-
-//     listJson.jsonContent = thing;
+//         };
+//     };
+// };
 
     // find the information within the API
 
-    // get the result back to the user: item's name, 
-    // description, image and locations.
-    const thingInfo = document.createElement('span');
-// });
+// get the result back to the user: item's name, description, image and locations.
